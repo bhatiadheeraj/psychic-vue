d52135a47e403d9134c459d4d9ae2990d95c4196
bhatiadheeraj <dheerajbhatia2010@gmail.com>
2020-08-04 22:18:11 -0400

    Finished The Vue instance tutorial and challenge :https://www.vuemastery.com/courses/intro-to-vue-js/vue-instance

---
+++ TUT-01-VUE-INSTANCE/main.js
@@ -0,0 +1,9 @@
+// Root part or vue instance
+var app = new Vue ({
+    // hooking up with div 
+    el: '#app',
+    // represent with expression '{{ }}'
+    data : {
+        product : 'Boots',
+        details : ['black','9.5','UK,US Sizes Available'],    }
+})
